2cbdcea2f18ef58ab0df62c7a6f8080762160eb7
imdeabsolute@gmail.com <imdeabsolute@gmail.com>
2025-07-29 19:59:06 +0000

    tested the normalizeurl function in crawl.js

---
+++ crawl.test.js
@@ -0,0 +1,29 @@
+const { normalizeURL } = require('./crawl.js');
+const { test, except, expect } = require('@jest/globals');
+
+test('normalizeURL strip https', () => {
+  const input = 'https://blog.boot.dev/path';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normalizeURL strip last slash', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('normalizeURL Capitalize', () => {
+  const input = 'https://BLOG.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+test('normalizeURL strip http', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normalizeURL(input);
+  const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
